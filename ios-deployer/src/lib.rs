f74176e18118f2b8292cbf4bb867f9d7ea96d54a
Wonway Song <sww4718168@163.com>
2021-02-19 22:43:31 +0800

    List devices tested.

---
+++ ios-deployer/src/lib.rs
@@ -0,0 +1,10 @@
+#[macro_use]
+extern crate log;
+
+// mod deployer;
+mod devices;
+mod mobile_device;
+mod mobile_device_errors;
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+mod mobile_device_sys;
